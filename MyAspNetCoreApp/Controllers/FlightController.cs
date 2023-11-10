cf9163bc98ce17337927f3372a02c6dfaa8776c2
AmilaJayamaha <amilajayamaha96@gmail.com>
2023-11-10 10:21:14 +0530

    Added flight management endpoints

---
+++ MyAspNetCoreApp/Controllers/FlightController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/flights")]
+[ApiController]
+public class FlightController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetFlights()
+    {
+       
+        return Ok(flights);
+    }
+
+    [HttpPost]
+    public IActionResult CreateFlight([FromBody] FlightModel flight)
+    {
+        
+        return CreatedAtAction("GetFlights", new { id = flight.Id }, flight);
+    }
+}
